1ee87e2cf54c7008d9d4bc071f1223e98d2685df
krzysiek.krowka <krzysztof.krowka@onet.eu>
2023-02-13 17:12:36 +0100

    znacznie ulepszono generowanie haseł

--- egzaminDesktop/Form1.cs
+++ egzaminDesktop/Form1.cs
@@ -12,32 +12,67 @@ namespace egzaminDesktop
         string znakiS = "!@#$%^&*()_+-=";
         string caleHaslo = " ";
 
-        //*************************
-        //nazwa funkcji: generujHaslo
-        //opis funkcji: funkcja generuje has³o
-        //parametry:    sender - podstawowy element forms
-        //              e - podstawowy element forms
-        //zwracany typ i opis: zwraca void
-        //autor: Krzysztof Krówka
-        //*************************
+        /// <summary>
+        /// Funkcja generuje has³o
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void generujHaslo(object sender, EventArgs e)
         {
             caleHaslo = " ";
+            
             Random random = new Random();
+            
             int liczbaZnakow = Convert.ToInt16(ileZnakow.Text);
+            
+            List<int> wolneMiejsca = new List<int>(liczbaZnakow);
+            for(int i = 0; i < wolneMiejsca.Capacity; i++)
+                wolneMiejsca.Add(i);
+            
             char[] haslo = new char[liczbaZnakow];
-            for (int i = 0; i < haslo.Length; i++)
-                haslo[i] =alfabetM[random.Next(0,25)];
             
-            if(maleIwielkie.Checked)
-                haslo[0] = alfabetW[random.Next(0, 25)];
-            
-            if(cyfry.Checked && haslo.Length > 1)
-                haslo[1] = Cyfry[random.Next(0, 10)];
-
-            if (znakiSpecjalne.Checked && haslo.Length>2)
-                haslo[2] = znakiS[random.Next(0, 14)];
-
+            int rodzaj = 0;
+            for (int i = 0; i < wolneMiejsca.Capacity; i++)
+            { 
+                int miejsce;
+                switch (rodzaj)
+                {
+                    case 0:
+                        miejsce = wolneMiejsca.ElementAt(random.Next(0, wolneMiejsca.Count));
+                        wolneMiejsca.Remove(miejsce);
+                        haslo[miejsce] = alfabetM[random.Next(0, 25)];
+                        break;
+                    case 1:
+                        if (maleIwielkie.Checked)
+                        {
+                            miejsce = wolneMiejsca.ElementAt(random.Next(0, wolneMiejsca.Count));
+                            wolneMiejsca.Remove(miejsce);
+                            haslo[miejsce] = alfabetW[random.Next(0, 25)];
+                        }
+                        else i--;
+                        break;
+                    case 2:
+                        if (cyfry.Checked)
+                        {
+                            miejsce = wolneMiejsca.ElementAt(random.Next(0, wolneMiejsca.Count));
+                            wolneMiejsca.Remove(miejsce);
+                            haslo[miejsce] = Cyfry[random.Next(0, 10)];
+                        }
+                        else i--;
+                        break;
+                    case 3:
+                        if (znakiSpecjalne.Checked)
+                        {
+                            miejsce = wolneMiejsca.ElementAt(random.Next(0, wolneMiejsca.Count));
+                            wolneMiejsca.Remove(miejsce);
+                            haslo[miejsce] = znakiS[random.Next(0, 14)];
+                        }
+                        else
+                            i--;
+                        break;
+                }
+                rodzaj = (rodzaj + 1) % 4;
+            }
             
             for (int i = 0; i < haslo.Length; i++)
                 caleHaslo +=haslo[i];
@@ -46,14 +81,11 @@ namespace egzaminDesktop
         }
 
 
-        //*************************
-        //nazwa funkcji: zatwierdz
-        //opis funkcji: funkcja wyœwietla dane pracownika
-        //parametry:    sender - podstawowy element forms
-        //              e - podstawowy element forms
-        //zwracany typ i opis: zwraca void
-        //autor: Krzysztof Krówka
-        //*************************
+        /// <summary>
+        /// Funkcja wyœwietla dane pracownika
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void zatwierdz(object sender, EventArgs e)
         {
             MessageBox.Show("Dane pracownika: " + imie.Text + " " + nazwisko.Text + " " + stanowisko.Text + " Has³o: " + caleHaslo);
